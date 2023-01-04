952f6b79db50f76e4ddf35891fcc83370d5df7c5
Madeline A <madeline.ashley2@gmail.com>
2023-01-04 17:28:38 -0600

    deploy from branch 2

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './App.css'
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import { CheckSession } from './services/Auth'
+import Nav from './components/Nav'
+import SignIn from './pages/SignIn'
+import Register from './pages/Register'
+import Cats from './pages/Cats'
+import About from './pages/About'
+import AddCat from './pages/AddCat'
+import Home from './pages/Home'
+import Triggers from './pages/Triggers'
+import Devices from './pages/Devices'
+import Dashboard from './components/Dashboard'
+import UserContext from './UserContext'
+import Header from './components/Header'
+import TriggerDetails from './pages/TriggerDetails'
+import Profile from './pages/Profile'
+
+
+
+ const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+  return (
+    <div>
+      <div>
+      <Header />
+          <Nav authenticated={authenticated} user={user} handleLogOut={handleLogOut}/>
+      </div>
+      <div>
+
+        <Routes>
+<Route path="/" element={<Home authenticated={authenticated} user={user}/>}/>
+<Route path="/dashboard" element={<Dashboard authenticated={authenticated} user={user}/>}/>
+<Route path="/login" element={<SignIn user={user} setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>}/>
+<Route path="/register" element={<Register/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/cats" element={<Cats user={user}/>}/>
+<Route path="/cats/add" element={<AddCat user={user}/>}/>
+<Route path="/profile" element={<Profile/>}/>
+<Route path="/triggers/details/:trigger_id" element={<TriggerDetails/>}/>
+<Route path="/triggers/:cat_id" element={<Triggers/>}/>
+        </Routes>
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-section"></div>
+
+      <div className="about">
+        <div className="about-large"><span>Hi,<br></br> This is me <br></br>and my cat <br></br>Feta!</span>
+          <img src="https://github.com/mashbash2150/LitterLogic-Frontend/blob/main/images/IMG_1436%20(1).JPG?raw=true" alt="" />   </div>  </div>
+
+      <div className="about-text">
+        <div className="about-med">LitterLogic Origin</div>
+        <div>Sure, Feta <em>IS</em> awesome, however I created this app not because I think cats are cool, but because I think electronics are!
+          My professional background has involved a great deal of work in automation and micro-electronics, so I set out to challenge myself by
+          incorporating my prior hardware expertise into my final project for General Assembly's Full Stack Software Engineering Immersive.
+          All aspects of this app, including the hardware sensor that supplies its data, were built from the ground up.  A few notes on it's components:
+
+          <div className="about-med">Hardware</div>
+          I put together a simple device consisiting of an ESP32 dev board, a PIR motion sensor, and LEDs (plus a few jumper wires and resistors).  Using the Arduino IDE, I wrote and uploaded a simple application to the device which allows it to send JSON objects to my Postgres backend.
+          The device detects minor shifts in environmental temperature, and interprets them as a motion event, changing the state of the sensor from low to high voltage (or vice versa).  When this occurs,a POST request is made to my express API, and further parsing of the payload happens downstream.
+          When placed in Feta's litterbox, the device will fire 'Enter' and 'Exit' events accordingly.  For addedd fun, the sensor behaves much like an airplane lavatory sign, illuminating a green cat when vacant, and a red cat when occupied.
+
+          <div className="about-med">Backend & API</div>
+
+          Hosted on Heroku for the time being, my Postgres backend stores parsed data from the sensor's API calls into a 'triggers' table, that supplies the information for my frontend tables. One area still
+          requiring improvments is the normalization of timestamps to facilitate simpler data visualization.
+          <div className="about-med">Frontend</div>
+          The React Data Table Component library was used to render trigger data in a table format.  While I wasn't able to implement chart libraries for the MVP version of this app, it is my goal in the near future to include charting in the insights portion of the dashboard.
+
+          <div className="about-text">While many improvements still need to be made, the project served its purpose as a fun exploration of the world of IOT - not to mention, it has inspired a number of other home automation projects that I will continue to noodle on in my spare time.
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
