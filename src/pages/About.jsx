1da93e60d521d3dec72d28cec69f9969cd7e6252
Madeline A <madeline.ashley2@gmail.com>
2023-01-10 17:35:06 -0600

    added sensor images to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,8 +20,10 @@ const About = () => {
           I put together a simple device consisiting of an ESP32 dev board, a PIR motion sensor, and LEDs (plus a few jumper wires and resistors).  Using the Arduino IDE, I wrote and uploaded a simple application to the device which allows it to send JSON objects to my Postgres backend.
           The device detects minor shifts in environmental temperature, and interprets them as a motion event, changing the state of the sensor from low to high voltage (or vice versa).  When this occurs,a POST request is made to my API, and further parsing of the payload happens downstream.
           When placed in Feta's litterbox, the device will fire 'Enter' and 'Exit' events accordingly.  For addedd fun, the sensor behaves much like an airplane lavatory sign, illuminating a green cat when vacant, and a red cat when occupied.
-          <div className="about-large">
-
+          <div className="about-img">
+            <img src="https://github.com/mashbash2150/LitterLogic-Frontend/blob/main/images/LLSensor_reduced.jpg?raw=true" alt="sensor" />
+            <img src="https://github.com/mashbash2150/LitterLogic-Frontend/blob/main/images/vacant2.gif?raw=true
+          " alt="hardware" />
           </div>
           <div className="about-med">Backend & API</div>
 
